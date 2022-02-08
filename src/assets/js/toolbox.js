1b64f14910d62608760bf5c7f171f0f687f31ec8
ZhenPeng <1936478563@qq.com>
2022-02-08 12:57:57 +0800

    fix a del bug

--- src/assets/js/toolbox.js
+++ src/assets/js/toolbox.js
@@ -64,22 +64,41 @@ function bindEditorSetting(store) {
 
     keyboardjs.bind('backspace', (e) => {
         if (e.target.innerHTML == "") {
-            let el = e.target.parentNode
-            //恢复聚焦
-            let childList = Array.from(document.getElementById("componentContainer").childNodes)
-            let position = childList.indexOf(el) - 1
-            childList[position].childNodes[0].focus()
-            //防止误删
-            new Promise((resolve, reject) => {
-                setTimeout(() => {
+            if (e.target.parentNode.childNodes.length > 1) {
+                //并列情况
+                let childList = e.target.parentNode.childNodes
+                let position = Array.from(childList).indexOf(e.target) - 1
+                childList[position].focus()
+                new Promise((resolve, reject) => {
+                    setTimeout(() => {
+                        resolve()
+                    }, 100)
+
+                }).then(() => {
+                    setCaret(childList[position])
+                })
+                //移除
+                let tempEl = e.target.parentNode
+                e.target.parentNode.removeChild(e.target);
+                applyWidth(tempEl)
+            } else {
+                let el = e.target.parentNode
+                //恢复聚焦
+                let childList = Array.from(document.getElementById("componentContainer").childNodes)
+                let position = childList.indexOf(el) - 1
+                childList[position].childNodes[0].focus()
+                //防止误删
+                new Promise((resolve, reject) => {
+                    setTimeout(() => {
+
+                        resolve()
+                    }, 100)
+                }).then(() => {
                     setCaret(childList[position].childNodes[0])
-                    resolve()
-                }, 100)
-            }).then(() => {
-                setCaret(childList[position].childNodes[0])
-            })
-            //移除
-            el.parentNode.removeChild(el);
+                })
+                //移除
+                el.parentNode.removeChild(el);
+            }
 
 
         }
